b1218e8adcaa2ebf633d09f4d4779b8fd07d1efb
Пользователь <kalmykhovae@gmail.com>
2024-02-09 18:03:56 +0300

    feat: add basic settings

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'semistandard',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: { ecmaVersion: 2018, sourceType: 'module' },
+  rules: {}
+};
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+
+const message: string = 'Hello node!!!';
+console.log(message);
+
